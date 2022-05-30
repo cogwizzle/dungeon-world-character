417c6fde868d40b2563a92e844550ec03806ee25
cogwizzle <joseph.fehrman@gmail.com>
2022-05-30 11:27:01 -0400

    Wiring up character form to character form observable.

--- src/components/character-form/character-form.js
+++ src/components/character-form/character-form.js
@@ -1,90 +1,55 @@
 import template from './character-form.html'
+import CharacterFormObservable from '../../state/character-form-observable'
 import './character-look/character-look'
 
 export class CharacterForm extends HTMLElement {
-  _characterName
-  _level
-  _xp
   constructor() {
     super()
   }
 
-  get characterName() {
-    return this._characterName
-  }
-
-  set characterName(value) {
-    this._characterName = value
-    const characterNameElement = this.querySelector('#character-name')
-    characterNameElement.value = this.characterName
-  }
-
-  get level() {
-    return this._level
-  }
-
-  set level(value) {
-    this._level = value
-    const levelElement = this.querySelector('#level')
-    levelElement.value = this.level
-  }
-
-  get xp() {
-    return this._xp
-  }
-
-  set xp(value) {
-    this._xp = value
-    const xpElement = this.querySelector('#xp')
-    xpElement.value = this.xp
-  }
-
   onCharacterNameChange = (event) => {
-    this._characterName = event.target.value
-    const characterNameElement = this.querySelector('#character-name')
-    characterNameElement.value = this.characterName
+    CharacterFormObservable.characterName = event.target.value
   }
 
   onLevelChange = (event) => {
-    this._level = event.target.value
-    const levelElement = this.querySelector('#level')
-    levelElement.value = this.level
+    CharacterFormObservable.level = event.target.value
   }
 
   onXpChange = (event) => {
-    this._xp = event.target.value
-    const xpElement = this.querySelector('#xp')
-    xpElement.value = this.xp
+    CharacterFormObservable.xp = event.target.value
   }
 
-  onLookChange = (event) => {
-    console.log(event.detail)
+  hydrate = (state) => {
+    const characterNameElement = this.querySelector('#character-name')
+    const levelElement = this.querySelector('#level')
+    const xpElement = this.querySelector('#xp')
+    characterNameElement.value = state.characterName || ''
+    levelElement.value = state.level || ''
+    xpElement.value = state.xp || 0
   }
 
   connectedCallback() {
     this.render()
+    CharacterFormObservable.subscribe(this.hydrate)
     const characterNameElement = this.querySelector('#character-name')
     const levelElement = this.querySelector('#level')
     const xpElement = this.querySelector('#xp')
-    const lookElement = this.querySelector('dw-character-look')
     characterNameElement.addEventListener('change', this.onCharacterNameChange)
     levelElement.addEventListener('change', this.onLevelChange)
     xpElement.addEventListener('change', this.onXpChange)
-    lookElement.addEventListener('dw-change', this.onLookChange)
   }
 
   disconnectedCallback() {
+    CharacterFormObservable.unsubscribe(this.hydrate)
     const characterNameElement = this.querySelector('#character-name')
     const levelElement = this.querySelector('#level')
     const xpElement = this.querySelector('#xp')
-    const lookElement = this.querySelector('dw-character-look')
     characterNameElement.removeEventListener(
       'change',
       this.onCharacterNameChange
     )
     levelElement.removeEventListener('change', this.onLevelChange)
     xpElement.removeEventListener('change', this.onXpChange)
-    lookElement.removeEventListener('dw-change', this.onLookChange)
   }
 
   render() {
--- src/components/character-form/character-look/character-look.js
+++ src/components/character-form/character-look/character-look.js
@@ -1,3 +1,4 @@
+import CharacterFormObservable from '../../../state/character-form-observable'
 import template from './character-look.html'
 
 export class CharacterLook extends HTMLElement {
@@ -27,7 +28,6 @@ export class CharacterLook extends HTMLElement {
 
   set body(value) {
     this._body = value
-    this.emit()
   }
 
   get eyes() {
@@ -36,7 +36,6 @@ export class CharacterLook extends HTMLElement {
 
   set eyes(value) {
     this._eyes = value
-    this.emit()
   }
 
   get hair() {
@@ -45,7 +44,6 @@ export class CharacterLook extends HTMLElement {
 
   set hair(value) {
     this._hair = value
-    this.emit()
   }
 
   get skin() {
@@ -54,110 +52,109 @@ export class CharacterLook extends HTMLElement {
 
   set skin(value) {
     this._skin = value
-    this.emit()
   }
 
   onBodyChange = (event) => {
     this._body = event.target.value
-    this.emit()
-    const bodyOther = this.querySelector('#body-other')
-    bodyOther.value = ''
-  }
-
-  onBodyOtherChange = (event) => {
-    this._body = event.target.value
-    this.emit()
-    const bodyElements = [
-      ...this.querySelectorAll('[name="body"][type="radio"]'),
-    ]
-    bodyElements.map((element) => (element.checked = false))
+    CharacterFormObservable.look = {
+      body: this._body,
+      eyes: this._eyes,
+      hair: this._hair,
+    }
   }
 
   onEyesChange = (event) => {
     this._eyes = event.target.value
-    this.emit()
-    const eyesOther = this.querySelector('#eyes-other')
-    eyesOther.value = ''
-  }
-
-  onEyesOtherChange = (event) => {
-    this._eyes = event.target.value
-    this.emit()
-    const eyesElements = [
-      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
-    ]
-    eyesElements.map((element) => (element.checked = false))
+    CharacterFormObservable.look = {
+      body: this._body,
+      eyes: this._eyes,
+      hair: this._hair,
+    }
   }
 
   onHairChange = (event) => {
     this._hair = event.target.value
-    this.emit()
-    const hairOther = this.querySelector('#hair-other')
-    hairOther.value = ''
+    CharacterFormObservable.look = {
+      body: this._body,
+      eyes: this._eyes,
+      hair: this._hair,
+    }
   }
 
-  onHairOtherChange = (event) => {
-    this._hair = event.target.value
-    this.emit()
+  hydrate = (state) => {
+    const look = state.look || {}
+    this._body = look.body || ''
+    this._eyes = look.eyes || ''
+    this._hair = look.hair || ''
+    const bodyElements = [
+      ...this.querySelectorAll('[name="body"][type="radio"]'),
+    ]
+    bodyElements.map(
+      (element) => (element.checked = look.body === element.value)
+    )
+    const bodyOther = this.querySelector('#body-other')
+    bodyOther.value = bodyElements.every(
+      (element) => element.value !== look.body
+    )
+      ? look.body
+      : ''
+    const eyesElements = [
+      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
+    ]
+    eyesElements.map(
+      (element) => (element.checked = look.eyes === element.value)
+    )
+    const eyesOther = this.querySelector('#eyes-other')
+    eyesOther.value = eyesElements.every(
+      (element) => element.value !== look.eyes
+    )
+      ? look.eyes
+      : ''
     const hairElements = [
       ...this.querySelectorAll('[name="hair"][type="radio"]'),
     ]
-    hairElements.map((element) => (element.checked = false))
+    hairElements.map(
+      (element) => (element.checked = look.hair === element.value)
+    )
+    const hairOther = this.querySelector('#hair-other')
+    hairOther.value = hairElements.every(
+      (element) => element.value !== look.hair
+    )
+      ? look.hair
+      : ''
   }
 
   connectedCallback() {
     this.render()
-    const bodyElements = [
-      ...this.querySelectorAll('[name="body"][type="radio"]'),
-    ]
-    const bodyOther = this.querySelector('#body-other')
-    const eyesElements = [
-      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
-    ]
-    const eyesOther = this.querySelector('#eyes-other')
-    const hairElements = [
-      ...this.querySelectorAll('[name="hair"][type="radio"]'),
-    ]
-    const hairOther = this.querySelector('#hair-other')
+    CharacterFormObservable.subscribe(this.hydrate)
+    const bodyElements = [...this.querySelectorAll('[name="body"]')]
+    const eyesElements = [...this.querySelectorAll('[name="eyes"]')]
+    const hairElements = [...this.querySelectorAll('[name="hair"]')]
     bodyElements.map((element) =>
       element.addEventListener('change', this.onBodyChange)
     )
-    bodyOther.addEventListener('change', this.onBodyOtherChange)
     eyesElements.map((element) =>
       element.addEventListener('change', this.onEyesChange)
     )
-    eyesOther.addEventListener('change', this.onEyesOtherChange)
     hairElements.map((element) =>
       element.addEventListener('change', this.onHairChange)
     )
-    hairOther.addEventListener('change', this.onHairOtherChange)
   }
 
   disconnectedCallback() {
-    const bodyElements = [
-      ...this.querySelectorAll('[name="body"][type="radio"]'),
-    ]
-    const bodyOther = this.querySelector('#body-other')
-    const eyesElements = [
-      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
-    ]
-    const eyesOther = this.querySelector('#eyes-other')
-    const hairElements = [
-      ...this.querySelectorAll('[name="hair"][type="radio"]'),
-    ]
-    const hairOther = this.querySelector('#hair-other')
+    CharacterFormObservable.unsubscribe(this.hydrate)
+    const bodyElements = [...this.querySelectorAll('[name="body"]')]
+    const eyesElements = [...this.querySelectorAll('[name="eyes"]')]
+    const hairElements = [...this.querySelectorAll('[name="hair"]')]
     bodyElements.map((element) =>
       element.removeEventListener('change', this.onBodyChange)
     )
-    bodyOther.addEventListener('change', this.onBodyOtherChange)
     eyesElements.map((element) =>
       element.removeEventListener('change', this.onEyesChange)
     )
-    eyesOther.removeEventListener('change', this.onEyesOtherChange)
     hairElements.map((element) =>
       element.removeEventListener('change', this.onHairChange)
     )
-    hairOther.removeEventListener('change', this.onHairOtherChange)
   }
 
   render() {
