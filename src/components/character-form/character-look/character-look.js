ba5aae37005e54a6a33c4f17a4b6b1440b66774e
cogwizzle <joseph.fehrman@gmail.com>
2022-05-29 22:17:45 -0400

    Adding other field to the character look component.

--- src/components/character-form/character-look/character-look.js
+++ src/components/character-form/character-look/character-look.js
@@ -60,47 +60,104 @@ export class CharacterLook extends HTMLElement {
   onBodyChange = (event) => {
     this._body = event.target.value
     this.emit()
+    const bodyOther = this.querySelector('#body-other')
+    bodyOther.value = ''
+  }
+
+  onBodyOtherChange = (event) => {
+    this._body = event.target.value
+    this.emit()
+    const bodyElements = [
+      ...this.querySelectorAll('[name="body"][type="radio"]'),
+    ]
+    bodyElements.map((element) => (element.checked = false))
   }
 
   onEyesChange = (event) => {
     this._eyes = event.target.value
     this.emit()
+    const eyesOther = this.querySelector('#eyes-other')
+    eyesOther.value = ''
+  }
+
+  onEyesOtherChange = (event) => {
+    this._eyes = event.target.value
+    this.emit()
+    const eyesElements = [
+      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
+    ]
+    eyesElements.map((element) => (element.checked = false))
   }
 
   onHairChange = (event) => {
     this._hair = event.target.value
     this.emit()
+    const hairOther = this.querySelector('#hair-other')
+    hairOther.value = ''
+  }
+
+  onHairOtherChange = (event) => {
+    this._hair = event.target.value
+    this.emit()
+    const hairElements = [
+      ...this.querySelectorAll('[name="hair"][type="radio"]'),
+    ]
+    hairElements.map((element) => (element.checked = false))
   }
 
   connectedCallback() {
     this.render()
-    const bodyElements = [...this.querySelectorAll('[name="body-shape"]')]
-    const eyesElements = [...this.querySelectorAll('[name="eyes"]')]
-    const hairElements = [...this.querySelectorAll('[name="hair"]')]
+    const bodyElements = [
+      ...this.querySelectorAll('[name="body"][type="radio"]'),
+    ]
+    const bodyOther = this.querySelector('#body-other')
+    const eyesElements = [
+      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
+    ]
+    const eyesOther = this.querySelector('#eyes-other')
+    const hairElements = [
+      ...this.querySelectorAll('[name="hair"][type="radio"]'),
+    ]
+    const hairOther = this.querySelector('#hair-other')
     bodyElements.map((element) =>
       element.addEventListener('change', this.onBodyChange)
     )
+    bodyOther.addEventListener('change', this.onBodyOtherChange)
     eyesElements.map((element) =>
       element.addEventListener('change', this.onEyesChange)
     )
+    eyesOther.addEventListener('change', this.onEyesOtherChange)
     hairElements.map((element) =>
       element.addEventListener('change', this.onHairChange)
     )
+    hairOther.addEventListener('change', this.onHairOtherChange)
   }
 
   disconnectedCallback() {
-    const bodyElements = this.querySelectorAll('[name="body"]')
-    const eyesElements = this.querySelectorAll('[name="eyes"]')
-    const hairElements = this.querySelectorAll('[name="hair"]')
+    const bodyElements = [
+      ...this.querySelectorAll('[name="body"][type="radio"]'),
+    ]
+    const bodyOther = this.querySelector('#body-other')
+    const eyesElements = [
+      ...this.querySelectorAll('[name="eyes"][type="radio"]'),
+    ]
+    const eyesOther = this.querySelector('#eyes-other')
+    const hairElements = [
+      ...this.querySelectorAll('[name="hair"][type="radio"]'),
+    ]
+    const hairOther = this.querySelector('#hair-other')
     bodyElements.map((element) =>
       element.removeEventListener('change', this.onBodyChange)
     )
+    bodyOther.addEventListener('change', this.onBodyOtherChange)
     eyesElements.map((element) =>
       element.removeEventListener('change', this.onEyesChange)
     )
+    eyesOther.removeEventListener('change', this.onEyesOtherChange)
     hairElements.map((element) =>
       element.removeEventListener('change', this.onHairChange)
     )
+    hairOther.removeEventListener('change', this.onHairOtherChange)
   }
 
   render() {
