6644c8d4b539760462d34f8f17d59bb713bc7ef9
cogwizzle <joseph.fehrman@gmail.com>
2022-06-02 21:04:20 -0400

    Implementing new section header.

--- src/components/character-form/character-look/character-look.js
+++ src/components/character-form/character-look/character-look.js
@@ -1,5 +1,6 @@
 import CharacterFormObservable from '../../../state/character-form-observable'
 import template from './character-look.html'
+import '../../section-header/section-header'
 
 export class CharacterLook extends HTMLElement {
   _body
