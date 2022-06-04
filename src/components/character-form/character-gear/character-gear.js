1d3fe9f59831f1888ac2db9fd557cd7ba465c843
cogwizzle <joseph.fehrman@gmail.com>
2022-06-04 15:25:27 -0400

    Filling in gear details.

--- src/components/character-form/character-gear/character-gear.js
+++ src/components/character-form/character-gear/character-gear.js
@@ -1,23 +1,40 @@
 import template from './character-gear.html'
+import CharacterFormObservable from '../../../state/character-form-observable'
+import '../../section-header/section-header'
 
 export class CharacterGear extends HTMLElement {
   constructor() {
     super()
   }
 
-  static get observedAttributes() {
-    return []
+  hydrate = (state) => {
+    this.querySelector('#gear-list').value = state.gear || []
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {}
+  onUpdate(event) {
+    CharacterFormObservable.gear = event.detail
+  }
 
   connectedCallback() {
     this.render()
+    CharacterFormObservable.subscribe(this.hydrate)
+    this.querySelector('#gear-list').addEventListener(
+      'dw-infinite-list-change',
+      this.onUpdate
+    )
   }
 
-  disconnectedCallback() {}
+  disconnectedCallback() {
+    CharacterFormObservable.unsubscribe(this.hydrate)
+    this.querySelector('#gear-list').removeEventListener(
+      'dw-infinite-list-change',
+      this.onUpdate
+    )
+  }
 
   render() {
     this.innerHTML = template
   }
 }
+
+customElements.define('dw-character-gear', CharacterGear)
