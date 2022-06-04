d0c0a852b2c33b5aadd82818f06cfd5749c923b3
cogwizzle <joseph.fehrman@gmail.com>
2022-06-04 15:02:32 -0400

    Creating character coin component.

---
+++ src/components/character-form/character-coin/character-coin.js
@@ -0,0 +1,47 @@
+import template from './character-coin.html'
+import '../../section-header/section-header'
+import CharacterFormObservable from '../../../state/character-form-observable'
+
+export class CharacterCoin extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  static get observedAttributes() {
+    return ['value']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'value') {
+      this.querySelector('#coin').innerHTML = newValue
+    }
+  }
+
+  hydrate = (state) => {
+    const coinElement = this.querySelector('#coin')
+    coinElement.value = state.coin || 0
+  }
+
+  connectedCallback() {
+    this.render()
+    CharacterFormObservable.subscribe(this.hydrate)
+    const coinElement = this.querySelector('#coin')
+    coinElement.addEventListener('change', (event) => {
+      CharacterFormObservable.coin = event.target.value
+    })
+  }
+
+  disconnectedCallback() {
+    CharacterFormObservable.unsubscribe(this.hydrate)
+    const coinElement = this.querySelector('#coin')
+    coinElement.removeEventListener('change', (event) => {
+      CharacterFormObservable.coin = event.target.value
+    })
+  }
+
+  render() {
+    this.innerHTML = template
+  }
+}
+
+customElements.define('dw-character-coin', CharacterCoin)
