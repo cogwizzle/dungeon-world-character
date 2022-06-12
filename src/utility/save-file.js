0b7dbb494efbaa62dce07fd04f56935bc714421c
cogwizzle <joseph.fehrman@gmail.com>
2022-06-12 15:31:38 -0400

    Creating a save file utility.

---
+++ src/utility/save-file.js
@@ -0,0 +1,9 @@
+const saveFile = (content, fileName, contentType) => {
+  const a = document.createElement('a')
+  const file = new Blob([content], { type: contentType })
+  a.href = URL.createObjectURL(file)
+  a.download = fileName
+  a.click()
+}
+
+export default saveFile
