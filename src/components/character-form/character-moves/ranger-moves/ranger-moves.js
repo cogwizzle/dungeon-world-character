447f3916fbc050750c8791de14be20f234b50947
cogwizzle <joseph.fehrman@gmail.com>
2022-06-16 19:32:39 -0400

    Adding support for all of the species for animal companion.

--- src/components/character-form/character-moves/ranger-moves/ranger-moves.js
+++ src/components/character-form/character-moves/ranger-moves/ranger-moves.js
@@ -1,7 +1,20 @@
 import { AbstractCharacterMoves } from '../abstract-character-moves'
 import template from './ranger-moves.html'
 
-const inputs = []
+const inputs = [
+  ['#animal-companion', 'animalCompanion'],
+  ['#animal-companion-name', 'animalCompanionName'],
+  ['#animal-companion-wolf', 'animalCompanionWolf'],
+  ['#animal-companion-bear', 'animalCompanionBear'],
+  ['#animal-companion-eagle', 'animalCompanionEagle'],
+  ['#animal-companion-dog', 'animalCompanionDog'],
+  ['#animal-companion-hawk', 'animalCompanionHawk'],
+  ['#animal-companion-cat', 'animalCompanionCat'],
+  ['#animal-companion-owl', 'animalCompanionOwl'],
+  ['#animal-companion-pigeon', 'animalCompanionPigeon'],
+  ['#animal-companion-rat', 'animalCompanionRat'],
+  ['#animal-companion-mule', 'animalCompanionMule'],
+]
 
 export class RangerMoves extends AbstractCharacterMoves {
   _inputs = inputs
