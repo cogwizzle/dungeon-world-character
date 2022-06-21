626c12d4a11d918d88f38c32bb61599ce6e19c2a
Cogwizzle <joseph.fehrman@gmail.com>
2022-06-20 20:19:45 -0400

    Hiding player moves until class is selected.

--- src/components/player-moves/player-moves.js
+++ src/components/player-moves/player-moves.js
@@ -1,16 +1,38 @@
+import CharacterFormObservable from '../../state/character-form-observable'
 import template from './player-moves.html'
 
 export class PlayerMoves extends HTMLElement {
+  _characterClass
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
   }
 
-  connectedCallback() {
-    this.render()
+  hydrate(state) {
+    this._characterClass = state.characterClass
+    if (this._characterClass && this._characterClass !== '') {
+      this.classList.remove('hidden')
+    } else if (!this.classList.contains('hidden')) {
+      this.classList.add('hidden')
+    }
   }
 
-  disconnectedCallback() {}
+  onMount() {
+    CharacterFormObservable.subscribe(this.hydrate.bind(this))
+  }
+
+  beforeUnmount() {
+    CharacterFormObservable.unsubscribe(this.hydrate.bind(this))
+  }
+
+  connectedCallback() {
+    this.render()
+    this.onMount()
+  }
+
+  disconnectedCallback() {
+    this.beforeUnmount()
+  }
 
   render() {
     this.shadowRoot.innerHTML = template
