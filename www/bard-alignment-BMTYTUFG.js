de09f33ee59612a14a3af4c5399827f3b10304d2
Cogwizzle <joseph.fehrman@gmail.com>
2022-06-23 20:13:30 -0400

    Building the project.

---
+++ www/bard-alignment-BMTYTUFG.js
@@ -0,0 +1,10 @@
+import"./chunk-YJ2DKIZS.js";var n=`<dw-vertical-selection\r
+  id="alignment"\r
+  title="alignment"\r
+  options='[\r
+    {"name": "good", "description": "Perform your art to aid someone else."},\r
+    {"name": "neutral", "description": "Avoid a conflict or defuse a tense situation."},\r
+    {"name": "chaotic", "description": "Spur others to significant and unplanned decisive action."}\r
+  ]'\r
+></dw-vertical-selection>\r
+`;export{n as default};
