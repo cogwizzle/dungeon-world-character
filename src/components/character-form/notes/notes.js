d9f9e4096f71ea9bc8034202680c8f23c0262b57
Cogwizzle <joseph.fehrman@gmail.com>
2022-06-25 15:20:24 -0400

    Adding support for notes storing values.

--- src/components/character-form/notes/notes.js
+++ src/components/character-form/notes/notes.js
@@ -1,24 +1,37 @@
 import template from './notes.html'
 
 export class Notes extends HTMLElement {
+  _value = ''
   constructor() {
     super()
     this.shadowRoot = this.attachShadow({ mode: 'open' })
   }
 
+  static get observedAttributes() {
+    return ['value']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'value' && oldValue !== newValue) {
+      this._value = newValue
+      this.shadowRoot.querySelector('#notes')?.innerHTML = newValue
+    }
+  }
+
   onMount() {
     this.shadowRoot
       .querySelector('#notes')
-      .addEventListener('keyup', this.onChange.bind(this))
+      ?.addEventListener('keyup', this.onChange.bind(this))
   }
 
   beforeUnmount() {
     this.shadowRoot
       .querySelector('#notes')
-      .removeEventListener('keyup', this.onChange.bind(this))
+      ?.removeEventListener('keyup', this.onChange.bind(this))
   }
 
   onChange(event) {
+    this._value = event.target.value
     this.dispatchEvent(
       new CustomEvent('notes-changed', { detail: event.target.value })
     )
@@ -35,6 +48,7 @@ export class Notes extends HTMLElement {
 
   render() {
     this.shadowRoot.innerHTML = template
+    this.shadowRoot.querySelector('#notes')?.innerHTML = this._value
   }
 }
 
