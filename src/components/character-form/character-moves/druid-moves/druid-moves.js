d5936722ff9307ab92588196a6702bd25aa519a2
Cogwizzle <joseph.fehrman@gmail.com>
2022-06-30 20:15:29 -0400

    Adding born of the soil.

--- src/components/character-form/character-moves/druid-moves/druid-moves.js
+++ src/components/character-form/character-moves/druid-moves/druid-moves.js
@@ -1,7 +1,20 @@
 import template from './druid-moves.html'
 import { AbstractCharacterMoves } from '../abstract-character-moves'
 
-const inputs = []
+const inputs = [
+  ['#born-of-the-soil', 'bornOfTheSoil'],
+  ['#the-great-forest', 'theGreatForest'],
+  ['#the-whispering-plains', 'theWhisperingPlains'],
+  ['#the-vast-desert', 'theVastDesert'],
+  ['#the-stinking-mire', 'theStinkingMire'],
+  ['#the-river-delta', 'theRiverDelta'],
+  ['#the-depths-of-the-earth', 'theDepthsOfTheEarth'],
+  ['#the-saphire-islands', 'theSaphireIslands'],
+  ['#the-open-sea', 'theOpenSea'],
+  ['#the-towering-mountains', 'theToweringMountains'],
+  ['#the-frozen-north', 'theFrozenNorth'],
+  ['#the-blasted-wasteland', 'theBlastedWasteland'],
+]
 
 export class DruidMoves extends AbstractCharacterMoves {
   _inputs = inputs
