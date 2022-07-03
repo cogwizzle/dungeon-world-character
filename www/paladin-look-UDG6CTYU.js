1adc37c7a251da1b2b3f2ce81238981de8543dab
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-03 10:47:39 -0400

    Production build.

---
+++ www/paladin-look-UDG6CTYU.js
@@ -0,0 +1,25 @@
+import"./chunk-YJ2DKIZS.js";var e=`<div class="flex flex-col">\r
+  <dw-section-header>LOOK</dw-section-header>\r
+  <div class="text-xs">\r
+    <div class="px-2 text-xs italic">\r
+      Choose one for each, or write your own:\r
+    </div>\r
+    <dw-character-look-group\r
+      title="body"\r
+      options='["fit", "bulky", "thin"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="eyes"\r
+      options='["kind", "fiery", "glowing"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="hair"\r
+      options='["bald", "styled", "helmet"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="holy-symbol"\r
+      options='["worn", "fancy"]'\r
+    ></dw-character-look-group>\r
+  </div>\r
+</div>\r
+`;export{e as default};
