bec162510e48b5aae4890bd255d648c61f08ef91
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-03 10:10:22 -0400

    Adding support for quest move.

--- src/components/character-form/character-moves/paladin-moves/paladin-moves.js
+++ src/components/character-form/character-moves/paladin-moves/paladin-moves.js
@@ -1,7 +1,15 @@
 import template from './paladin-moves.html'
 import { AbstractCharacterMoves } from '../abstract-character-moves'
+import '../../../section-header/section-header'
 
-const inputs = []
+const inputs = [
+  ['#quest', 'quest'],
+  ['#quest-slay', 'questSlay'],
+  ['#quest-defend', 'questDefend'],
+  ['#quest-discover', 'questDiscover'],
+  ['#quest-direction', 'questDirection'],
+  ['#quest-invulnerability', 'questInvulnerability'],
+]
 
 export class PaladinMoves extends AbstractCharacterMoves {
   _inputs = inputs
