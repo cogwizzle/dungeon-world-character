24356d18e76a710d7111dcfe0310e41a20514909
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-04 20:20:44 -0400

    Prod build.

---
+++ www/thief-alignment-XKSWLV5Z.js
@@ -0,0 +1,10 @@
+import"./chunk-YJ2DKIZS.js";var t=`<dw-vertical-selection\r
+  id="alignment"\r
+  title="alignment"\r
+  options='[\r
+    {"name": "chaotic", "description": "Leap into danger without a plan."},\r
+    {"name": "neutral", "description": "Avoid detection or infiltrate a location."},\r
+    {"name": "evil", "description": "Shift danger or blame from yourself to someone else."}\r
+  ]'\r
+></dw-vertical-selection>\r
+`;export{t as default};
---
+++ www/thief-moves-ZZ7LZ7FA.js
@@ -0,0 +1,367 @@
+import{a as o}from"./chunk-2XGXHTGB.js";import"./chunk-7Z555Q4H.js";import{a as e}from"./chunk-YJ2DKIZS.js";var a=`<div>\r
+  <dw-section-header>STARTING MOVES</dw-section-header>\r
+  <div class="grid grid-cols-2 gap-2">\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="trap-expert" name="trap-expert" />\r
+        <label for="trap-expert">Trap Expert</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you spend a moment to survey a dangerous area, roll+DEX. \u2022 On a\r
+          10+, hold 3. \u2022 On a 7-9, hold 1. Spend your hold as you walk through\r
+          the area to ask these questions:\r
+        </div>\r
+        <ul class="ml-5 text-sm list-disc">\r
+          <li>Is there a trap here and if so, what activates it?</li>\r
+          <li>What does the trap do when activated?</li>\r
+          <li>What else is hidden here?</li>\r
+        </ul>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="flexible-morals" name="flexible-morals" />\r
+        <label for="flexible-morals">Flexible Morals</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When someone tries to detect your alignment you can tell them any\r
+          alignment you like.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input\r
+          type="checkbox"\r
+          id="tricks-of-the-trade"\r
+          name="tricks-of-the-trade"\r
+        />\r
+        <label for="tricks-of-the-trade">Tricks of the Trade</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you pick locks or pockets or disable traps, roll+DEX. \u2022 On a 10+,\r
+          you do it, no problem. \u2022 On a 7-9, you still do it, but the GM will\r
+          offer you two options between suspicion, danger, or cost.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="poisoner" name="poisoner" />\r
+        <label for="poisoner">Poisoner</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          You've mastered the care and use of a poison. Choose a poison from the\r
+          list below; that poison is no longer dangerous for you to use. You\r
+          also start with three uses of the poison you choose. Whenever you have\r
+          time to gather materials and a safe place to brew you can make three\r
+          uses of the poison you choose for free. Note that some poisons are\r
+          applied, meaning you have to carefully apply it to the target or\r
+          something they eat or drink. Touch poisons just need to touch the\r
+          target, they can even be used on the blade of a weapon.\r
+        </div>\r
+        <ul class="ml-5 text-sm list-disc">\r
+          <li>Oil of Tagit: (applied) The target falls into a light sleep.</li>\r
+          <li>\r
+            Bloodweed: (touch) The target deals -1d4 damage ongoing until cured.\r
+          </li>\r
+          <li>\r
+            Goldenroot: (applied) The target treats he next creature they see as\r
+            a trusted ally, until proved otherwise\r
+          </li>\r
+          <li>\r
+            Serpent's Tears: (touch) Anyone dealing damage to the target rolls\r
+            twice and takes the better result.\r
+          </li>\r
+        </ul>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="backstab" name="backstab" />\r
+        <label for="backstab">Backstab</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you attack a surprised or defenseless enemy with a melee weapon,\r
+          you can choose to deal your damage or roll+DEX. \u2022 On a 10+, choose\r
+          two. \u2022 On a 7-9, choose one:\r
+        </div>\r
+        <ul class="ml-5 text-sm list-disc">\r
+          <li>You don't get into melee with them</li>\r
+          <li>You deal your damage+1d6</li>\r
+          <li>\r
+            You create an advantage, +1 forward to you or an ally acting on it\r
+          </li>\r
+          <li>Reduce their armor by 1 until they repair it</li>\r
+        </ul>\r
+      </div>\r
+    </div>\r
+  </div>\r
+  <dw-section-header>ADVANCED MOVES</dw-section-header>\r
+  <div class="text-sm italic">\r
+    When you gain a level from 2-10, you may choose from these moves.\r
+  </div>\r
+  <div class="grid grid-cols-2 gap-2">\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="cheap-shot" name="cheap-shot" />\r
+        <label for="cheap-shot">Cheap Shot</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When using a precise or hand weapon, your Backstab deals an extra +1d6\r
+          damage.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="poison-master" name="poison-master" />\r
+        <label for="poison-master">Poison Master</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          After you've used a poison once it's no longer dangerous for you to\r
+          use.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="cautious" name="cautious" />\r
+        <label for="cautious">Cautious</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you use Trap Expert you always get +1 hold, even on a 6-.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="envenom" name="envenom" />\r
+        <label for="envenom">Envenom</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          You can apply even complex poisons with a pinprick. When you apply a\r
+          poison that's not dangerous for you to use to your weapon it's touch\r
+          instead of applied.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="wealth-and-taste" name="wealth-and-taste" />\r
+        <label for="wealth-and-taste">Wealth-and-Taste</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you make a show of flashing around your most valuable possession,\r
+          choose someone present. They will do anything they can to obtain your\r
+          item or one like it.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="brewer" name="brewer" />\r
+        <label for="brewer">Brewer</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you have time to gather materials and a safe place to brew you\r
+          can create three doses of any one poison you've used before.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="shoot-first" name="shoot-first" />\r
+        <label for="shoot-first">Shoot-First</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          You're never caught by surprise. When an enemy would get the drop on\r
+          you, you get to act first instead.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="connections" name="connections" />\r
+        <label for="connections">Connections</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you put out word to the criminal underbelly about something you\r
+          want or need, roll+CHA. \u2022 On a 10+, someone has it, just for you. \u2022 On\r
+          a 7-9, you'll have to settle for something close or it comes with\r
+          strings attached, your call.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="underdog" name="underdog" />\r
+        <label for="underdog">Underdog</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>When you're outnumbered, you have +1 armor.</div>\r
+      </div>\r
+    </div>\r
+  </div>\r
+  <hr />\r
+  <div class="text-sm italic">\r
+    When you gain a level from 6-10, you may choose from these moves.\r
+  </div>\r
+  <div class="grid grid-cols-2 gap-2">\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="dirty-fighter" name="dirty-fighter" />\r
+        <label for="dirty-fighter">Dirty Fighter</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div class="text-sm italic">Replaces: Cheap Shot</div>\r
+        <div>\r
+          When using a precise or hand weapon, your Backstab deals an extra +1d8\r
+          damage and all other attacks deal +1d4 damage.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="alchemist" name="alchemist" />\r
+        <label for="alchemist">Alchemist</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div class="text-sm italic">Replaces: Brewer</div>\r
+        <div>\r
+          When you have time to gather materials and a safe place to brew you\r
+          can create three doses of any poison you've used before. Alternately\r
+          you can describe the effects of a poison you'd like to create. The GM\r
+          will tell you that you can create it, but with one or more caveats:\r
+        </div>\r
+        <ul class="ml-5 list-disc text-sm">\r
+          <li>It will only work under specific circumstances.</li>\r
+          <li>The best you can manage is a weaker version.</li>\r
+          <li>It'll take a while to take effect.</li>\r
+          <li>It'll have obvious side effects.</li>\r
+        </ul>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input\r
+          type="checkbox"\r
+          id="extremely-cautious"\r
+          name="extremely-cautious"\r
+        />\r
+        <label for="extremely-cautious">Extremely-Cautious</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div class="text-sm italic">Replaces: Cautious</div>\r
+        <div>\r
+          When you use use Trap Expert you always get +1 hold, even on a 6-. On\r
+          a 12+ you get 3 hold and the next time you come near a trap the GM\r
+          will immediately tell you what it does, what triggers it, who set it,\r
+          and how you can use it to your advantage.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="serious-underdog" name="serious-underdog" />\r
+        <label for="serious-underdog">Serious Underdog</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div class="text-sm italic">Replaces: Underdog</div>\r
+        <div>\r
+          You have +1 armor. When you're outnumbered, you have +2 armor instead.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="escape-route" name="escape-route" />\r
+        <label for="escape-route">Escape Route</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you're in too deep and need a way out, name your escape route and\r
+          roll+DEX. \u2022 On a 10+ you're gone. \u2022 On a 7-9 you can stay or go, but\r
+          if you go it costs you: leave something behind or take something with\r
+          you, the GM will tell you what.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="evasion" name="evasion" />\r
+        <label for="evasion">Evasion</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you Defy Danger on a 12+, you transcend the danger. You not only\r
+          do what you set out to, but the GM will offer you a better outcome,\r
+          true beauty, or a moment of grace.\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="disguise" name="disguise" />\r
+        <label for="disguise">Disguise</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you have time and materials you can create a disguise that will\r
+          fool anyone into thinking you're another creature of about the same\r
+          size and shape. Your actions can give you away but your appearance\r
+          won't\r
+        </div>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input type="checkbox" id="heist" name="heist" />\r
+        <label for="heist">Heist</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          When you take time to make a plan to steal something, name the thing\r
+          you want to steal and ask the GM these questions. When acting on the\r
+          answers you and your allies take +1 forward.\r
+        </div>\r
+        <ul class="ml-5 list-disc text-sm">\r
+          <li>Who will notice it's missing?</li>\r
+          <li>What's its most powerful defense?</li>\r
+          <li>Who will come after it?</li>\r
+          <li>Who else wants it?</li>\r
+        </ul>\r
+      </div>\r
+    </div>\r
+    <div class="flex flex-col col-span-2 lg:col-span-1">\r
+      <div class="flex flex-row col-span-2 lg:col-span-1">\r
+        <input\r
+          type="checkbox"\r
+          id="strong-arm-true-aim"\r
+          name="strong-arm-true-aim"\r
+        />\r
+        <label for="strong-arm-true-aim">Strong Arm, True Aim</label>\r
+      </div>\r
+      <div class="flex flex-col">\r
+        <div>\r
+          You can throw any melee weapon, using it to volley. A thrown melee\r
+          weapon is gone; you can never choose to reduce ammo on a 7-9.\r
+        </div>\r
+      </div>\r
+    </div>\r
+  </div>\r
+</div>\r
+`;var i=[["#trap-expert","trapExpert"],["#flexible-morals","flexibleMorals"],["#tricks-of-the-trade","tricksOfTheTrade"],["#poisoner","poisoner"],["#backstab","backstab"],["#cheap-shot","cheapShot"],["#poison-master","poisonMaster"],["#cautious","cautious"],["#envenom","envenom"],["#wealth-and-taste","wealthAndTaste"],["#brewer","brewer"],["#shoot-first","shootFirst"],["#connections","connections"],["#underdog","underdog"],["#dirty-fighter","dirtyFighter"],["#alchemist","alchemist"],["#extremely-cautious","extremelyCautious"],["#serious-underdog","seriousUnderdog"],["#escape-route","escapeRoute"],["#evasion","evasion"],["#disguise","disguise"],["#heist","heist"],["#strong-arm-true-aim","strongArmTrueAim"]],l=class extends o{constructor(){super();e(this,"_inputs",i);e(this,"_template",a)}};customElements.define("dw-thief-moves",l);export{l as ThiefMoves};
