f2adc717107c1824ab1a43ae2750dbcf92fd9988
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-09 11:02:16 -0400

    Creating a labeled checkbox component.

---
+++ src/components/labeled-checkbox/labeled-checkbox.js
@@ -0,0 +1,50 @@
+import template from './labeled-checkbox.html'
+const templateEl = document.createElement('template')
+templateEl.innerHTML = template
+
+export class LabeledCheckbox extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  static get observedAttributes() {
+    return ['key', 'label']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return
+    switch (name) {
+      case 'key':
+      case 'label':
+        this.onMount()
+        break
+      default:
+        break
+    }
+  }
+
+  onMount() {
+    const key = this.getAttribute('key')
+    const label = this.getAttribute('label')
+    const input = this.querySelector('input')
+    const labelEl = this.querySelector('label')
+    if (labelEl) {
+      labelEl.innerHTML = label
+      labelEl.setAttribute('for', key)
+    }
+    if (!input) return
+    input.setAttribute('name', key)
+    input.setAttribute('id', key)
+  }
+
+  connectedCallback() {
+    this.render()
+    this.onMount()
+  }
+
+  render() {
+    this.appendChild(templateEl.content.cloneNode(true))
+  }
+}
+
+customElements.define('dw-labeled-checkbox', LabeledCheckbox)
