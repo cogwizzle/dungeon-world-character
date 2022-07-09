a88222f92a086f75324db5372fc08df3683f51b6
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-09 12:53:02 -0400

    Production build.

---
+++ www/cleric-alignment-DXOW2ZVR.js
@@ -0,0 +1,10 @@
+import"./chunk-YJ2DKIZS.js";var r=`<dw-vertical-selection\r
+  id="alignment"\r
+  title="alignment"\r
+  options='[\r
+    {"name": "good", "description": "Endanger yourself to heal another."},\r
+    {"name": "lawful", "description": "Endanger yourself following the precepts of your church or god."},\r
+    {"name": "evil", "description": "Harm another to prove the superiority of your church or god."}\r
+  ]'\r
+></dw-vertical-selection>\r
+`;export{r as default};
---
+++ www/cleric-look-F5EQKNGC.js
@@ -0,0 +1,25 @@
+import"./chunk-YJ2DKIZS.js";var t=`<div class="flex flex-col">\r
+  <dw-section-header>LOOK</dw-section-header>\r
+  <div class="text-xs">\r
+    <div class="px-2 text-xs italic">\r
+      Choose one for each, or write your own:\r
+    </div>\r
+    <dw-character-look-group\r
+      title="body"\r
+      options='["thin", "knobby", "flabby"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="eyes"\r
+      options='["kind", "sharp", "sad"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="hair"\r
+      options='["strange", "tonsure", "bald"]'\r
+    ></dw-character-look-group>\r
+    <dw-character-look-group\r
+      title="clothes"\r
+      options='["flowing robes", "habit", "common"]'\r
+    ></dw-character-look-group>\r
+  </div>\r
+</div>\r
+`;export{t as default};
