3ea411d232d4f33ff4d67283f9452e7bd999b061
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-09 11:02:32 -0400

    Adding support for diety move.

--- src/components/character-form/character-moves/cleric-moves/cleric-moves.js
+++ src/components/character-form/character-moves/cleric-moves/cleric-moves.js
@@ -1,8 +1,22 @@
 import template from './cleric-moves.html'
 import { AbstractCharacterMoves } from '../abstract-character-moves'
 import '../../../section-header/section-header'
+import '../../../detailed-checkbox/detailed-checkbox'
+import '../../../labeled-checkbox/labeled-checkbox'
 
-const inputs = []
+const inputs = [
+  ['#diety', 'diety'],
+  ['#diety-healing-and-restoration', 'dietyHealingAndRestoration'],
+  ['#diety-bloody-conquest', 'dietyBloodyConquest'],
+  ['#diety-civilization', 'dietyCivilization'],
+  ['#diety-knowledge-and-hidden-things', 'dietyKnowledgeAndHiddenThings'],
+  ['#diety-the-downtrodden-and-forgotten', 'dietyTheDowntroddenAndForgotten'],
+  ['#diety-what-lies-beneath', 'dietyWhatLiesBeneath'],
+  ['#diety-petition-suffering', 'dietyPetitionSuffering'],
+  ['#diety-petition-gaining-secrets', 'dietyPetitionGainingSecrets'],
+  ['#diety-petition-offering', 'dietyPetitionOffering'],
+  ['#diety-petition-personal-victory', 'dietyPetitionPersonalVictory'],
+]
 
 export class ClericMoves extends AbstractCharacterMoves {
   _inputs = inputs
