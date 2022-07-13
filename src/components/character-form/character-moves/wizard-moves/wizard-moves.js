5a58c0fa443393735f15848672d22cef7ddb26db
Cogwizzle <joseph.fehrman@gmail.com>
2022-07-12 20:05:01 -0400

    Wiring up wizard staring spells.

--- src/components/character-form/character-moves/wizard-moves/wizard-moves.js
+++ src/components/character-form/character-moves/wizard-moves/wizard-moves.js
@@ -1,8 +1,15 @@
 import template from './wizard-moves.html'
 import { AbstractCharacterMoves } from '../abstract-character-moves'
 import '../../../section-header/section-header'
+import '../../../detailed-checkbox/detailed-checkbox'
 
-const inputs = []
+const inputs = [
+  ['#cast-a-spell', 'castASpell'],
+  ['#spellbook', 'spellbook'],
+  ['#spell-defense', 'spellDefense'],
+  ['#prepare-spells', 'prepareSpells'],
+  ['#ritual', 'ritual'],
+]
 
 export class WizardMoves extends AbstractCharacterMoves {
   _inputs = inputs
