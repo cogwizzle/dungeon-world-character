b86ff50a4b524d08c137ab6c58b1af4b789c4f87
cogwizzle <joseph.fehrman@gmail.com>
2022-07-20 16:25:25 -0400

    Fixing cache rendering issue in character moves component.

--- src/components/character-form/character-moves/character-moves.js
+++ src/components/character-form/character-moves/character-moves.js
@@ -30,8 +30,9 @@ export class CharacterMoves extends HTMLElement {
   }
 
   connectedCallback() {
-    this.render()
-    CharacterFormObservable.subscribe(this.hydrate.bind(this))
+    this.render().then(() =>
+      CharacterFormObservable.subscribe(this.hydrate.bind(this))
+    )
   }
 
   disconnectedCallback() {
@@ -39,45 +40,51 @@ export class CharacterMoves extends HTMLElement {
   }
 
   async render() {
-    switch (this._characterClass) {
-      case supportedClasses.Fighter:
-        await import('./fighter-moves/fighter-moves')
-        this.innerHTML = '<dw-fighter-moves></dw-fighter-moves>'
-        break
-      case supportedClasses.Ranger:
-        await import('./ranger-moves/ranger-moves')
-        this.innerHTML = '<dw-ranger-moves></dw-ranger-moves>'
-        break
-      case supportedClasses.Bard:
-        await import('./bard-moves/bard-moves')
-        this.innerHTML = '<dw-bard-moves></dw-bard-moves>'
-        break
-      case supportedClasses.Druid:
-        await import('./druid-moves/druid-moves')
-        this.innerHTML = '<dw-druid-moves></dw-druid-moves>'
-        break
-      case supportedClasses.Paladin:
-        await import('./paladin-moves/paladin-moves')
-        this.innerHTML = '<dw-paladin-moves></dw-paladin-moves>'
-        break
-      case supportedClasses.Thief:
-        await import('./thief-moves/thief-moves')
-        this.innerHTML = '<dw-thief-moves></dw-thief-moves>'
-        break
-      case supportedClasses.Cleric:
-        await import('./cleric-moves/cleric-moves')
-        this.innerHTML = '<dw-cleric-moves></dw-cleric-moves>'
-        break
-      case supportedClasses.Wizard:
-        await import('./wizard-moves/wizard-moves')
-        this.innerHTML = '<dw-wizard-moves></dw-wizard-moves>'
-        break
-      default:
-        await import('./other-moves/other-moves')
-        this.innerHTML = '<dw-other-moves></dw-other-moves>'
-        break
-    }
-    this.updateDom()
+    if (!this._characterClass) return
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        switch (this._characterClass) {
+          case supportedClasses.Fighter:
+            await import('./fighter-moves/fighter-moves')
+            this.innerHTML = '<dw-fighter-moves></dw-fighter-moves>'
+            break
+          case supportedClasses.Ranger:
+            await import('./ranger-moves/ranger-moves')
+            this.innerHTML = '<dw-ranger-moves></dw-ranger-moves>'
+            break
+          case supportedClasses.Bard:
+            await import('./bard-moves/bard-moves')
+            this.innerHTML = '<dw-bard-moves></dw-bard-moves>'
+            break
+          case supportedClasses.Druid:
+            await import('./druid-moves/druid-moves')
+            this.innerHTML = '<dw-druid-moves></dw-druid-moves>'
+            break
+          case supportedClasses.Paladin:
+            await import('./paladin-moves/paladin-moves')
+            this.innerHTML = '<dw-paladin-moves></dw-paladin-moves>'
+            break
+          case supportedClasses.Thief:
+            await import('./thief-moves/thief-moves')
+            this.innerHTML = '<dw-thief-moves></dw-thief-moves>'
+            break
+          case supportedClasses.Cleric:
+            await import('./cleric-moves/cleric-moves')
+            this.innerHTML = '<dw-cleric-moves></dw-cleric-moves>'
+            break
+          case supportedClasses.Wizard:
+            await import('./wizard-moves/wizard-moves')
+            this.innerHTML = '<dw-wizard-moves></dw-wizard-moves>'
+            break
+          default:
+            await import('./other-moves/other-moves')
+            this.innerHTML = '<dw-other-moves></dw-other-moves>'
+            break
+        }
+        this.updateDom()
+        resolve()
+      })
+    })
   }
 }
 
