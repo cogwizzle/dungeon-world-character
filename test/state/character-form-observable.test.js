38e62394cf1a55d46ae90d0af826937ccacdff47
Joe Fehrman <joseph.fehrman@gmail.com>
2022-07-23 00:32:13 +0000

    Adding test for save, loadCharacter, notify, subscribe, and set characterName.

---
+++ test/state/character-form-observable.test.js
@@ -0,0 +1,54 @@
+import { expect } from '@esm-bundle/chai'
+import ChracterFormObservable from '../../src/state/character-form-observable.js'
+
+beforeEach(() => {
+  ChracterFormObservable.reset()
+})
+
+it('Given I have an instance of the character-form-observable when I save the observable then the character form observable responds with the current state.', () => {
+  const result = ChracterFormObservable.save()
+  expect(result).to.deep.equal({})
+})
+
+it('Given I have an instance of the character-form-observable when I load a character then I set the state of the observable and notify all subscribes.', () => {
+  const state = {
+    characterName: 'Bartleby',
+  }
+  let result
+  ChracterFormObservable.subscribe((state) => (result = state))
+  ChracterFormObservable.loadCharacter(state)
+  expect(result).to.deep.equal(state)
+})
+
+it('Given I have an instance of the character-form-observable when I notify a user then it saves the current state of the character-form-observable in localStorage under the key "character."', () => {
+  const state = {
+    characterName: 'Bartleby',
+  }
+  ChracterFormObservable.loadCharacter(state)
+  ChracterFormObservable.notify()
+  const result = JSON.parse(localStorage.getItem('character'))
+  expect(result).to.deep.equal(state)
+})
+
+it('Given I have an instance of the character-form-observable when I subscribe to the observable then my function is added to the list of observables and the current state is passed to that new function.', () => {
+  let result
+  ChracterFormObservable.subscribe((state) => {
+    result = state
+  })
+  expect(result).to.deep.equal({})
+  expect(ChracterFormObservable._observers).to.have.length(1)
+})
+
+it('Given I have an instance of the character-form-observable when I set the characterName property then the state should be updated with the characterName and the notify function should be called.', () => {
+  let result
+  ChracterFormObservable.subscribe((state) => {
+    result = state
+  })
+  ChracterFormObservable.characterName = 'Bartleby'
+  expect(ChracterFormObservable._state).to.deep.equal({
+    characterName: 'Bartleby',
+  })
+  expect(result).to.deep.equal({
+    characterName: 'Bartleby',
+  })
+})
