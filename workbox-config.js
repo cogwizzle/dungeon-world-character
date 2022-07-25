043ad11fbf480a6ede0f3139b486378ed98b1b9d
Joe Fehrman <joseph.fehrman@gmail.com>
2022-07-25 18:34:38 +0000

    Fixing workbox config.

--- workbox-config.js
+++ workbox-config.js
@@ -1,23 +1,21 @@
 module.exports = {
-	globDirectory: 'www/',
-	globPatterns: [
-		'**/*.{svg,js,png,html,json,css}'
-	],
-	swDest: 'www/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/,
-		/^dungeon/,
-		/^world/,
-		/^character/,
-		/^sheet/,
-		/^play/,
-		/^bard/,
-		/^cleric/,
-		/^druid/,
-		/^paladin/,
-		/^ranger/,
-		/^thief/,
-		/^wizard/
-	]
-};
+  globDirectory: 'client/www/',
+  globPatterns: ['**/*.{svg,png,html,json,css}', '**/*.index.js'],
+  swDest: 'www/sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/,
+    /^dungeon/,
+    /^world/,
+    /^character/,
+    /^sheet/,
+    /^play/,
+    /^bard/,
+    /^cleric/,
+    /^druid/,
+    /^paladin/,
+    /^ranger/,
+    /^thief/,
+    /^wizard/,
+  ],
+}
