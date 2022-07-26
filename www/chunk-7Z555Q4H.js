02a32a801970475f0f306a63462dfa5edcae1475
Joe Fehrman <joseph.fehrman@gmail.com>
2022-07-26 00:43:38 +0000

    Dead code.

--- www/chunk-7Z555Q4H.js
+++
@@ -1,4 +0,0 @@
-var t=`<h1 class="bg-black text-white px-2">\r
-  <slot></slot>\r
-</h1>\r
-`;var e=class extends HTMLElement{constructor(){super()}connectedCallback(){this.render()}render(){let n=this.innerHTML;this.innerHTML=t,this.querySelector("slot").innerHTML=n}};customElements.define("dw-section-header",e);
