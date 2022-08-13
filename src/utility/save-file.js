5fbcd0f80c72b8653bf24831d74dc2c85b7872e5
Joe Fehrman <joseph.fehrman@gmail.com>
2022-08-13 14:46:00 +0000

    Add JSDoc to safe-file utility Fixes #215

--- src/utility/save-file.js
+++ src/utility/save-file.js
@@ -1,3 +1,10 @@
+/**
+ * Save content to a file using filename and a mime type.
+ * @param {string} content Content to be stored in the file.
+ * @param {string} fileName Name of the file.
+ * @param {string} contentType Mimetype.
+ * @returns {void}
+ */
 const saveFile = (content, fileName, contentType) => {
   const a = document.createElement('a')
   const file = new Blob([content], { type: contentType })
