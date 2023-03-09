85ca7632e7a48285d97ad12e5fa14d3943fa4b41
cogwizzle <joseph.fehrman@gmail.com>
2023-03-08 21:37:35 -0500

    feat(side-effect): Moving reset functionality to side effect subscription.

--- src/state/character-form-persistence.js
+++ src/state/character-form-persistence.js
@@ -1,6 +1,15 @@
 import CharacterFormObservable from './character-form-observable'
 
 CharacterFormObservable.subscribe((characterForm) => {
+  // If the character form has been reset.
+  if (Object.keys(characterForm).length === 0) {
+
+    // Remove the character form from local storage.
+    localStorage.removeItem('character')
+
+    // Reload the page.
+    window.location.reload()
+  }
   // Save the character form to local storage each time a change happens.
-  localStorage.setItem('characterForm', JSON.stringify(characterForm))
+  localStorage.setItem('character', JSON.stringify(characterForm))
 })
